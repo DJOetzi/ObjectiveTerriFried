6d866c3afae3d96422bb7f9345231defa9285105
DJ::Ötzi <maxgabri@icloud.com>
2024-11-04 16:34:38 +0100

    add autoreleasepool

--- ObjectiveTerriFried/main.m
+++ ObjectiveTerriFried/main.m
@@ -1,47 +1,42 @@
-//
-//  main.m
-//  ObjectiveTerriFried
-//
-//  Created by Maximilian Gabriel on 04.11.24.
-//
-
 #import <Foundation/Foundation.h>
 #include <raylib.h>
 
 int main(int argc, const char * argv[]) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    
-    
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    @autoreleasepool {
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+        
+        
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+        SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+        //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // Main game loop
+        while (!WindowShouldClose())    // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            // TODO: Update your variables here
+            //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+                ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+                DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+            EndDrawing();
+            //----------------------------------------------------------------------------------
+        }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        CloseWindow();        // Close window and OpenGL context
+        //--------------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    
     return 0;
 }
