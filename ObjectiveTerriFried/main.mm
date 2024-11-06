b88c40fb4e28e331fcd7fe9b5f07fcadc89608d8
DJ::Ötzi <maxgabri@icloud.com>
2024-11-06 22:00:19 +0100

    Update main.mm
    change player style

--- ObjectiveTerriFried/main.mm
+++ ObjectiveTerriFried/main.mm
@@ -1,6 +1,17 @@
 #import <Foundation/Foundation.h>
 #include <raylib.h>
 
+#import "engine/include/Platform.hpp"
+#import "engine/include/Player.hpp"
+
+Platform* platforms[4] = {
+    [[Platform alloc] initWithIndex:1],
+    
+};
+
+// to port: [platforms[0] getX] + [platforms[0] getWidth]/2 - 26/2, [platforms[0] getY] - [player getHeight], 26, 32
+Player* player [[Player alloc] initWithParams:<#(double)#> andYinit:<#(double)#> andWidthinit:<#(int)#> andHeightinit:<#(int)#> andOnPlatforminit:<#(bool)#> andVelocity:<#(Vector2)#>];
+
 auto main(int argc, const char * argv[]) -> int {
     const int screenWidth = 800;
     const int screenHeight = 450;
