9d8649ac67bf6b099ff5562051cc7adafaab34e7
DJ::Ötzi <maxgabri@icloud.com>
2024-12-04 10:04:45 +0100

    add: resource management

--- ObjectiveTerriFried/main.mm
+++ ObjectiveTerriFried/main.mm
@@ -11,6 +11,24 @@
 auto main(int argc, const char* argv[]) -> int {
     srand((unsigned)time(NULL));
     
+    Texture2D playerSprite = LoadTexture("resources/egg.png");
+    Texture2D lavaSprite = LoadTexture("resources/lava.png");
+    Texture2D platformSprite = LoadTexture("resources/platform.png");
+    Texture2D coinSprite = LoadTexture("resources/coin.png");
+    Texture2D scoreBoxSprite = LoadTexture("resources/scorebox.png");
+    Texture2D logo = LoadTexture("resources/logo.png");
+    Texture2D splashEggSprite = LoadTexture("resources/splash_egg.png");
+    
+    Sound fxLaunch = LoadSound("resources/launch.wav");
+    Sound fxClick = LoadSound("resources/click.wav");
+    Sound fxDeath = LoadSound("resources/die.wav");
+    Sound fxCoin = LoadSound("resources/coin.wav");
+    Sound fxSplash = LoadSound("resources/splash.wav");
+    Sound fxSelect = LoadSound("resources/select.wav");
+    Font font = LoadFontEx("resources/font.otf", 64, 0, NULL);
+    
+    Image egg = LoadImage("resources/egg.png");
+    
     @autoreleasepool {
         Platform* platforms[4] = {
             [[Platform alloc] initWithIndex:0],
@@ -30,11 +48,25 @@ auto main(int argc, const char* argv[]) -> int {
         
         ScoreManager* scoreMan = [[ScoreManager alloc] init];
         [scoreMan setHighscoreInt: [SaveManager loadStorageValue:0]];
+        [scoreMan resetScore];
+        
+        int mouseDownX = 0;
+        int mouseDownY = 0;
+        double lavaY = [Constants SCREEN_HEIGHT] - 32;
+        double timer = 0;
+        double splashTimer = 0;
+        bool firstTime = true;
+        bool playedSplash = false;
+        bool playedSelect = false;
         
         InitWindow([Constants SCREEN_WIDTH], [Constants SCREEN_HEIGHT], "TerriFried");
+        SetWindowIcon(egg);
+        InitAudioDevice();
+        SetMasterVolume(0.3f);
 
         SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
         //--------------------------------------------------------------------------------------
+        
 
         // Main game loop
         while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -60,6 +92,23 @@ auto main(int argc, const char* argv[]) -> int {
         //--------------------------------------------------------------------------------------
         CloseWindow();        // Close window and OpenGL context
         //--------------------------------------------------------------------------------------
+        
+        UnloadTexture(playerSprite);
+        UnloadTexture(lavaSprite);
+        UnloadTexture(platformSprite);
+        UnloadTexture(coinSprite);
+        UnloadTexture(scoreBoxSprite);
+        UnloadTexture(logo);
+        UnloadTexture(splashEggSprite);
+        UnloadSound(fxClick);
+        UnloadSound(fxLaunch);
+        UnloadSound(fxDeath);
+        UnloadSound(fxCoin);
+        UnloadSound(fxSplash);
+        UnloadSound(fxSelect);
+        UnloadFont(font);
+        CloseAudioDevice();
+        CloseWindow();
     }
     
     return 0;
