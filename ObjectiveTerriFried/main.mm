bd750a42fd33d7de93f60aa2e80e88a9d4d4dc1c
DJ::Ötzi <maxgabri@icloud.com>
2024-12-04 11:52:22 +0100

    Update main.mm

--- ObjectiveTerriFried/main.mm
+++ ObjectiveTerriFried/main.mm
@@ -74,11 +74,11 @@ auto main(int argc, const char* argv[]) -> int {
             // Draw
             //----------------------------------------------------------------------------------
             BeginDrawing();
-
+            {
                 ClearBackground(RAYWHITE);
 
                 DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+            }
             EndDrawing();
             //----------------------------------------------------------------------------------
         }
